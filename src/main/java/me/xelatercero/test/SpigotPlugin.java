1f7b7d5c190197dd20a26f079fea2d895f1999a8
xelatercero <braga.alex57@gmail.com>
2020-09-30 00:59:34 +0200

    Add files via upload

---
+++ src/main/java/me/xelatercero/test/SpigotPlugin.java
@@ -0,0 +1,15 @@
+package me.xelatercero.test;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SpigotPlugin extends JavaPlugin {
+    @Override
+    public void onDisable() {
+        // Don't log disabling, Spigot does that for you automatically!
+    }
+
+    @Override
+    public void onEnable() {
+       
+    }
+}
